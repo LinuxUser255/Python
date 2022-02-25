eb0cb4c853285c40d78a136768327f4e0a522b99
Chris <atnl@protonmail.com>
2022-02-25 08:09:52 -0500

    Update p1.py

--- Python_OOP/Extra/p1.py
+++ Python_OOP/Extra/p1.py
@@ -1,32 +1,58 @@
 #!/usr/bin/env python3
 
+# use the mypy tool to check type hints
 import math
 
 class Point:
     """
+    This class represents a point in two-dimensional geo corordinates.
+    
     The following initialization function to the Point Class
     requires the user, upon the Point Object's instantiation,
     to assign values to x & y
     """
     def __init__(self, x: float, y: float) ->None:
-        self.x = x
-        self.y = y
+        """
+        Initialize the position of a new point. The x & y coordinates can be specified.
+        However, if they are not, then the point defaults to the original.
+
+        :param x: float x-cordinate
+        :param y: float x-coordinate
+        """
+        self.move(x, y)
 
     # The first method needs accept arguments
     # x & y that set a value on the self
     def move(self, x: float, y: float) -> None:
-        self.x = 0
-        self.y = 0
+        """
+        Move the point to a new location in 2D space.
+
+        :param x: float x-cordinate
+        :param y: float x-coordinate
+        """
+        self.x = x
+        self.y = y
+
 
     def reset(self) -> None:
-        """Call the move method above"""
+        """
+        Reset the point back to it's geometric origin by
+        calling the move method above.
+        """
         self.move(0, 0)
 
     # Lastly, calculate the Euclidian distance
     def calculate_distance(self, other: "Point") -> float:
+        """
+        Calculate the Euclidian distance from this point
+        to a second point passed as a parameter
+        :param other: Point instance
+        :return: float distance
+        """
         return math.hypot(self.x - other.x, self.y - other.y)
+        
 
-
+        
 # Create a point instance:
-point = Point(3, 5)
-print(point.x, point.y)
+# point = Point(3, 5)
+# print(point.x, point.y)
