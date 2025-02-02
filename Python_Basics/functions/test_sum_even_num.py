e302f2bfa18c71b362245eabfbc4e0d7fdd09ea7
Chris <atnl@protonmail.com>
2025-02-02 16:59:08 -0500

    Create test_sum_even_num.py

---
+++ Python_Basics/functions/test_sum_even_num.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# run this script to test sum_even_nums_timed.py
+# ./sum_even_nums_timed.py
+# imports from the function `def sum_even_numbers(numbers) from sum_even_nums_timed.py
+# and runs it against this file
+from sum_even_nums_timed import sum_even_numbers
+
+# Test cases
+print(sum_even_numbers([]))  # Output: 0
+#print(sum_even_numbers([1]))  # Output: 0
+#print(sum_even_numbers([2]))  # Output: 2
+#print(sum_even_numbers([1, 2, 3, 4, 5, 6]))  # Output: 12
+#print(sum_even_numbers([-2, 1, 4, -5, 6]))  # Output: 8
